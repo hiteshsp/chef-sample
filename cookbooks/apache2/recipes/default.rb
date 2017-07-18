91dfdbb3fd0259e1ca01218c81aa1a30b3c9bbc1
hitesh <hitesh@localhost.localdomain>
2017-07-18 14:36:11 +0530

    Adding cookbooks

---
+++ cookbooks/apache2/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: apache2
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe "install_apache2"
---
+++ cookbooks/apache2/recipes/install_jdk.rb
@@ -0,0 +1,2 @@
+apt_package 'default-jre'
+apt_package 'default-jdk'
